10a087fbcd2411f8ddaa88121460d2dd1605a32c
nguyendai217 <nguyendai2171998@gmail.com>
2020-11-01 14:55:55 +0700

    add manager news

--- src/main/java/com/wru/onthi/entity/News.java
+++ src/main/java/com/wru/onthi/entity/News.java
@@ -1,6 +1,7 @@
 package com.wru.onthi.entity;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "news")
@@ -13,8 +14,29 @@ public class News {
     @Column(name = "title")
     private String title;
 
-    @Column(name = "content")
+    @Column(name = "content", columnDefinition="LONGTEXT")
     private String content;
+
+    @Column(name = "insert_date")
+    private Date insertDate;
+
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    @Column(name = "insert_by")
+    private String insertBy;
+
+    @Column(name = "update_by")
+    private String updateBy;
+
+    @Column(name = "views")
+    private Integer views;
+
+    @Column(name = "tag")
+    private String tag;
+
+    @Column(name = "image")
+    private String image;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
@@ -62,5 +84,61 @@ public class News {
 
     public void setUserNews(User userNews) {
         this.userNews = userNews;
+    }
+
+    public Date getInsertDate() {
+        return insertDate;
+    }
+
+    public void setInsertDate(Date insertDate) {
+        this.insertDate = insertDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getInsertBy() {
+        return insertBy;
+    }
+
+    public void setInsertBy(String insertBy) {
+        this.insertBy = insertBy;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
---
+++ src/main/java/com/wru/onthi/entity/Question.java
@@ -0,0 +1,88 @@
+package com.wru.onthi.entity;
+
+import org.springframework.stereotype.Controller;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "question")
+public class Question {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "ans_a")
+    private String ansA;
+
+    @Column(name = "ans_b")
+    private String ansB;
+
+    @Column(name = "ans_c")
+    private String ansC;
+
+    @Column(name = "ans_d")
+    private String ansD;
+
+    @Column(name = "ans_correct")
+    private String ansCorrect;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAnsA() {
+        return ansA;
+    }
+
+    public void setAnsA(String ansA) {
+        this.ansA = ansA;
+    }
+
+    public String getAnsB() {
+        return ansB;
+    }
+
+    public void setAnsB(String ansB) {
+        this.ansB = ansB;
+    }
+
+    public String getAnsC() {
+        return ansC;
+    }
+
+    public void setAnsC(String ansC) {
+        this.ansC = ansC;
+    }
+
+    public String getAnsD() {
+        return ansD;
+    }
+
+    public void setAnsD(String ansD) {
+        this.ansD = ansD;
+    }
+
+    public String getAnsCorrect() {
+        return ansCorrect;
+    }
+
+    public void setAnsCorrect(String ansCorrect) {
+        this.ansCorrect = ansCorrect;
+    }
+}
